ef342041e8a9e6ae1fa835c0b8883198c6a0c3d2
Drisya Ponmari <drisyaponmari@gmail.com>
2025-04-20 14:41:09 +0530

    standard

---
+++ timus/vol1/1082.cpp
@@ -0,0 +1,15 @@
+
+// Boost my confidence
+#include <iostream>
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cout << i << " ";
+    cout << endl;
+    return 0;
+}
