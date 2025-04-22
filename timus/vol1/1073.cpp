2c43e50cb61cb319e5790e23166300bf4ef48aca
Drisya Ponmari <drisyaponmari@gmail.com>
2025-04-22 22:17:45 +0530

    on2 solution

---
+++ timus/vol1/1073.cpp
@@ -0,0 +1,27 @@
+// Boost my confidence
+#include <iostream>
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+int main(){
+   int n;
+   cin >> n;
+   int dp[n+1];
+   dp[0] = 0;
+   int t = sqrt(n);
+   for(int i = 1; i <= n; i++){
+        int st = sqrt(i);
+        if(st * st == i){
+            dp[i] = 1;
+        }else{
+           int temp = dp[1] + dp[i-1];
+           for(int k = 2; k <= (i-1)/2; k++){
+               temp = min(temp, dp[k]+dp[i-k]);
+           }
+           dp[i] = temp;
+        }
+   }
+   cout << dp[n] << endl;
+    return 0;
+}
